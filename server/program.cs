64c364e249ae2f974ebd2c2046a88d7a11ee11ed
vekys <vekysek12@gmail.com>
2026-09-22 13:20:50 +0300

    Commit 11:

--- server/program.cs
+++ server/program.cs
@@ -1,3 +1,18 @@
-void main(){
-      
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("  Лабораторная работа №4");
+        Console.WriteLine("ФИО: Сташенко Д.Н.");
+        Console.WriteLine(DateTime.Now);
+        Console.WriteLine("Нажмите Enter для выхода...");
+        Console.WriteLine("Меню");
+        int r2 = int.Parse(Console.ReadLine()!);
+        switch (r2)
+        {
+            case 1
+        }
+    }
 }
